45fc7a138738c02212e7cb50ecb7689202f14b71
Luchluch <luc.dachary@numericable.fr>
2013-10-01 08:47:09 -0700

    Adding minimal connection script. Adding phpwebsocket class to set up a basic server.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+try
+{
+	//Assurons-nous que la classe WebSocket existe sur notre navigateur
+	if(!window.WebSocket)
+		throw "Impossible d'utiliser WebSocket. Votre navigateur ne l'implémente pas.";
+
+	var host = "127.0.0.1";
+	var port = 12345;
+	var target = "/SoWebSockets/php/test.php";
+	
+	var url = "ws://" + host + ":" + port + target;
+	//DEV
+	console.log("Tentative de connexion à ");
+	console.log(url);
+	var ws = new WebSocket(url);
+
+	if(undefined === ws)
+		throw "Impossible de créer un point de sortie.";
+
+	ws.onopen = function()
+	{
+		console.log("La communication est établie !");
+	}
+}
+catch(str)
+{
+	console.log("Erreur inattendue : " + str);
+}
